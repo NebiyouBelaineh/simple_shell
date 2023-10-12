cbb6708d07fc15ce0e2d67cc8fadffa92b340e71
root <bayiemeseret@gmail.com>
2023-10-12 12:32:07 +0000

    add _getenv.c

---
+++ _getenv.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _getenv - gets the enviorment variable required without using getenv()
+* function
+* @name: name of enviornment variable we want to find
+* Return: Enviorment variable key=value string or NULL if it does not exist.
+*/
+char *_getenv(const char *name)
+{
+	unsigned int index = 0;
+	char *token, *temp, *result;
+
+	if (name == NULL)
+		return (NULL);
+
+	while (__environ[index] != NULL)
+	{
+		temp = malloc(_strlen(__environ[index]) + 1);
+		_strcpy(temp, __environ[index]);
+		token = _strtok(temp, "=");
+
+		if (_strcmp(token, name) == 0)
+		{
+			token = _strtok(NULL, "=");
+			result = malloc(_strlen(token) + 1);
+			_strcpy(result, token);
+			free(temp);
+			return (result);
+		}
+		index++;
+		free(temp);
+	}
+
+	return (NULL);
+}
