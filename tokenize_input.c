fc8258fdcd7793b8f830426e684e1f67d2295249
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-10-18 13:39:37 +0300

    Add support for comments and handle spaces

--- tokenize_input.c
+++ tokenize_input.c
@@ -13,43 +13,52 @@
 int tokenize_input(char *line, char *arr_token[], int *index_ptr)
 {
 	int index;
-	char *token;
+	char *token, *delimiter = " \t";
 
-	if (line[0] == '\n' || line[0] == '\0')
-	{
-		free(line);
+	if (line[0] == '\n')
 		return (1);
-	}
-	line[_strcspn(line, "\n")] = 0;
-
-	if ((_strcmp(line, "env") == 0) && (_strlen(line) == _strlen("env")))
-	{
-		_printenv();
-		free(line);
+	if (_is_space(line))
 		return (1);
-	}
-	else if ((_strcmp(line, "exit") == 0)  && (_strlen(line) == _strlen("exit")))
-	{
-		free(line);
-		exit(EXIT_SUCCESS);
-	}
-	token = _strtok(line, " ");
+	if (line[0] == '#')
+		return (1);
+	token = _strtok(line, delimiter);
 	for (index = 0; token != NULL; index++)
 	{
+		/*Handles comments*/
+		if (_strcmp(token, "#") == 0)
+			break;
 		arr_token[index] = malloc((_strlen(token) * sizeof(char)) + 1);
 		if (arr_token[index] == NULL)
 		{
-			perror("Error: malloc:");
+			errno = ENOMEM;
+			perror("Error");
 			free_mem(index_ptr, arr_token);
 			free(line);
 			exit(EXIT_FAILURE);
 		}
 		_strcpy(arr_token[index], token);
-		token = _strtok(NULL, " ");
 		*index_ptr = index;
+		token = _strtok(NULL, delimiter);
 	}
 	arr_token[index] = NULL;
 	*index_ptr = index;
-	free(line);
 	return (0);
+}
+
+/**
+ * _is_space - checks if string is filled with space only
+ * @str: string to be checked
+ * Return: 0 if not all spaces, 1 otherwise
+*/
+int _is_space(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if (str[index] != ' ' && str[index] != '\t')
+			return (0);
+		index++;
+	}
+	return (1);
 }
