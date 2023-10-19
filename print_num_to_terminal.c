e7a3f4826b920c276eabaa90edfffccc1c0d3ba0
root <bayiemeseret@gmail.com>
2023-10-19 01:58:29 +0000

    some update to print_num_to_terminal

--- print_num_to_terminal.c
+++ print_num_to_terminal.c
@@ -1,6 +1,43 @@
 #include "main.h"
 
 /**
+* print_num_err - writes the character c to stderr
+* @c: The character to print
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int print_num_err(char c)
+{
+	return (write(STDERR_FILENO, &c, 1));
+}
+/**
+* print_number_err - prints integer 'n' using _putchar()
+* @n: integer argument passed to _putchar()
+* Return: void
+*/
+void print_number_err(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		num = -n;
+		print_to_stderr("-");
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num >= 10)
+	{
+		print_number_err(num / 10);
+	}
+
+	print_num_err((num % 10) + '0');
+}
+
+/**
 * print_num - writes the character c to stdout
 * @c: The character to print
 * Return: On success 1.
