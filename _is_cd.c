920116d4467dbd424a25d813f17378e25d04d693
root <bayiemeseret@gmail.com>
2023-10-19 01:54:12 +0000

    update to _is_cd

--- _is_cd.c
+++ _is_cd.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
+void handle_cd_err(char **arr_token, char *p_name, int *line_counter);
 /**
 * _is_cd - handles change directory commands
 * @arr_token: an array of strings with commands passed to the terminal
+* @p_name: program name
+* @line_counter: pointer to the number of line counter
 * Return: 0 for success, -1 for error
 */
-int _is_cd(char **arr_token)
+int _is_cd(char **arr_token, char *p_name, int *line_counter)
 {
 	char *pwd, *cwd;
 
@@ -39,9 +42,37 @@ int _is_cd(char **arr_token)
 		cwd = getcwd(NULL, 0);
 		_setenv("PWD", cwd, OVERWRITE);
 		/*Need to handle error on invalid directories and errors*/
-		chdir(arr_token[1]);
+		if (chdir(arr_token[1]) == -1)
+		{
+			handle_cd_err(arr_token, p_name, line_counter);
+			free(cwd);
+			free(pwd);
+			return (2);
+		}
 		free(cwd);
 		free(pwd);
+		return (0);
 	}
-	return (1);
+}
+
+/**
+ * handle_cd_err - prints error mesasge along with associated program name,
+ * line number and command passed to the terminal
+ * @arr_token: an array of string commands
+ * @p_name: program name
+ * @line_counter: pointer to the number of line counter
+ * Return: void.
+ */
+void handle_cd_err(char **arr_token, char *p_name, int *line_counter)
+{
+			print_to_stderr(p_name);
+			print_to_stderr(": ");
+			print_num_err(*line_counter);
+			print_to_stderr(": ");
+			print_to_stderr(arr_token[0]);
+			print_to_stderr(": ");
+			print_to_stderr("can't cd to ");
+			print_to_stderr(arr_token[1]);
+			if (isatty(_fileno(stdin)))
+				print_to_stderr("\n");
 }
